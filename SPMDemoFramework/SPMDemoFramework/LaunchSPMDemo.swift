29963a18460cc7cc6560070a6d046dd8e5cd12ca
reemabhalani <reema.bhalani@gmail.com>
2023-06-15 16:20:26 -0700

    Make function public.

--- SPMDemoFramework/SPMDemoFramework/LaunchSPMDemo.swift
+++ SPMDemoFramework/SPMDemoFramework/LaunchSPMDemo.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public class LaunchSPMDemo {
     
-    func launchView () {
-        print("Hello SPM Framework")
+    public func launchView () {
+        print("Hello SPM Framework!!")
     }
 }
