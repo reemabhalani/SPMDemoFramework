8e620d99c7b015400c35d4574a39eda2b80a7435
reemabhalani <reema.bhalani@gmail.com>
2023-06-15 18:02:00 -0700

    Update Launch class

--- SPMDemoFramework/SPMDemoFramework/LaunchSPMDemo.swift
+++ SPMDemoFramework/SPMDemoFramework/LaunchSPMDemo.swift
@@ -8,8 +8,13 @@
 import Foundation
 
 public class LaunchSPMDemo {
+    public init() {
+    }
+    
+    public var helloText: String?
     
     public func launchView () {
+        print(helloText)
         print("Hello SPM Framework!!")
     }
 }
